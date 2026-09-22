741c00f7be8cd867a6bfcccd1f08b3e8aed9de65
sayuprc <bepdielibe+github@gmail.com>
2026-09-22 21:04:49 +0900

    feat: Event の生成 PHP クライアントを追加

--- src/app/server/Generated/Admin/lib/Model/EventStatusValue.php
+++ src/app/server/Generated/Admin/lib/Model/EventStatusValue.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PermissionValue
+ * EventStatusValue
  *
  * PHP version 8.1
  *
@@ -30,47 +30,21 @@ namespace OpenAPI\Admin\Client\Model;
 use \OpenAPI\Admin\Client\ObjectSerializer;
 
 /**
- * PermissionValue Class Doc Comment
+ * EventStatusValue Class Doc Comment
  *
  * @category Class
- * @description 権限の値
+ * @description 活動の開催状態。通常開催は値を持たず、延期・中止のみ表現する
  * @package  OpenAPI\Admin\Client
  * @author   OpenAPI Generator team
  * @link     https://openapi-generator.tech
  */
-enum PermissionValue: string
+enum EventStatusValue: int
 {
     /**
      * Possible values of this enum
      */
-    case READ_ADMIN_USER = 'read_admin_user';
+    case NUMBER_1 = 1;
 
-    case WRITE_ADMIN_USER = 'write_admin_user';
-
-    case READ_PERSON = 'read_person';
-
-    case WRITE_PERSON = 'write_person';
-
-    case READ_SONG = 'read_song';
-
-    case WRITE_SONG = 'write_song';
-
-    case READ_MEDIA = 'read_media';
-
-    case WRITE_MEDIA = 'write_media';
-
-    case READ_RELEASE = 'read_release';
-
-    case WRITE_RELEASE = 'write_release';
-
-    case READ_VENUE = 'read_venue';
-
-    case WRITE_VENUE = 'write_venue';
-
-    case READ_EVENT = 'read_event';
-
-    case WRITE_EVENT = 'write_event';
+    case NUMBER_2 = 2;
 
 }
-
-
